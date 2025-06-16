b8cc62bd6df4a4cd4bed1edfe950913640513fe9
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-06-16 15:41:41 -0300

    feat: creating hibernate relationship between book and category

--- src/main/java/com/example/library_system/domain/category/Category.java
+++ src/main/java/com/example/library_system/domain/category/Category.java
@@ -1,11 +1,17 @@
 package com.example.library_system.domain.category;
 
 
+import java.util.List;
+
+import com.example.library_system.domain.book.Book;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -26,6 +32,9 @@ public class Category {
 
     private int code;
 
+    @OneToMany(mappedBy="category", cascade=CascadeType.REMOVE, orphanRemoval=true)
+    private List<Book> book;
+
     public String getName() {
         return this.name;
     }
@@ -40,6 +49,14 @@ public class Category {
 
     public void setCode(int code) {
         this.code = code;
+    }
+
+    public List<Book> getBook() {
+        return this.book;
+    }
+
+    public void setBook(List<Book> book) {
+        this.book = book;
     }
 
     public Category(RequestCategory requestCategory) {
