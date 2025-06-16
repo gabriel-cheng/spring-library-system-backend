c289b8c849f6b6b65660c81d6782769b63c4db17
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-06-16 15:40:43 -0300

    feat: creating hibernate relationship between author and book

--- src/main/java/com/example/library_system/domain/author/Author.java
+++ src/main/java/com/example/library_system/domain/author/Author.java
@@ -1,12 +1,17 @@
 package com.example.library_system.domain.author;
 
 import java.util.Date;
+import java.util.List;
 
+import com.example.library_system.domain.book.Book;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -33,6 +38,9 @@ public class Author {
     private String biography;
 
     private String nacionality;
+
+    @OneToMany(mappedBy="author", cascade=CascadeType.REMOVE, orphanRemoval=true)
+    private List<Book> book;
 
     public String getAuthorId() {
         return this.authorId;
@@ -80,6 +88,14 @@ public class Author {
 
     public void setNacionality(String nacionality)  {
         this.nacionality = nacionality;
+    }
+
+    public List<Book> getBook() {
+        return this.book;
+    }
+
+    public void setBook(List<Book> book)  {
+        this.book = book;
     }
 
     public Author(RequestAuthor requestAuthor) {
