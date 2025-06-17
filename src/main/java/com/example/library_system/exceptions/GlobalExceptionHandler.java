bb30d8d2667384f381adb5a16dffe4e9b9065a8e
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-06-17 15:21:32 -0300

    feat: creating exceptions

---
+++ src/main/java/com/example/library_system/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.library_system.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("An unexpected error ocurred. Please, try again later!");
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(ex.getMessage());
+    }
+
+}
