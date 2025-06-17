1554c0a01f835fb83853b8fbf8d45b9909cae1fa
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-06-17 15:21:55 -0300

    feat: creating book repository

---
+++ src/main/java/com/example/library_system/domain/loanbook/LoanBookRepository.java
@@ -0,0 +1,5 @@
+package com.example.library_system.domain.loanbook;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanBookRepository extends JpaRepository<LoanBook, String> { }
